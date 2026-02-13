6a84e4f7c1a971a925bdb05d5e0e6c9c756c739a
yuryk200 <yuriyk2025@gmail.com>
2026-02-13 06:40:14 +0000

    demo video

--- chemapp/chemsolve2/app/src/androidTest/java/com/example/chemsolve2/ExampleInstrumentedTest.java
+++ chemapp/chemsolve2/app/src/androidTest/java/com/example/chemsolve2/ExampleInstrumentedTest.java
@@ -19,7 +19,8 @@ import static org.junit.Assert.*;
 public class ExampleInstrumentedTest 
 {
     @Test
-    public void useAppContext() {
+    public void useAppContext() 
+    {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.chemsolve2", appContext.getPackageName());
